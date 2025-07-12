60ac4e09b699134c0c42830b87c94f46765033d0
mSev7n <pythagorasm7@gmail.com>
2025-07-12 15:30:01 +0100

    added functionality to the buttons ...so they get displayed to screen and operated on...also added comments

--- script/script.js
+++ script/script.js
@@ -21,11 +21,55 @@ const operations = {
     "/" : divide
 };
 
-let a = 0;
-let b = 0;
-let operator;
+let a = null;
+let b = null;
+let operator = null;
+let shouldClearDisplay = false;
+
+let displayContent = null;
 
 function operate(a, b, operator){
     let operation = operations[operator];
     return operation(a, b);
 }
+
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll(".digits");
+
+const allButtons = document.querySelectorAll(".operations");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        if (shouldClearDisplay) {
+            displayContent = "";
+            display.textContent = displayContent;
+        }
+        display.textContent += button.textContent;
+        displayContent = display.textContent;
+
+    })
+});
+
+allButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const value = button.textContent;
+        displayContent = parseFloat(display.textContent);
+
+            if (a === null) {
+                a = displayContent;
+                operator = value;
+                shouldClearDisplay = true;
+            }
+            else if (operator && b === null) {
+                b = displayContent;
+                a = operate(a, b, operator);
+                display.textContent = a;
+                b = null;
+                operator = value;
+                shouldClearDisplay = true;
+            }
+            else {
+                operator = value;
+            }
+    })
+});
